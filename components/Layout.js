795a8ae1c28cea79b5a750f32dd36fee11498a4d
udulaTharupathi99 <udulatharupathi333@gmail.com>
2022-03-16 09:07:23 +0530

    udula

--- components/Layout.js
+++ components/Layout.js
@@ -4,13 +4,13 @@ import styles from '../styles/Home.module.css'
 export default function Layout({children}) {
   return (
       <>
+        <nav className={styles.nav}> <h2>Chuck Norris Jokes</h2></nav>
         <div className={styles.container}>
-            <main>
-            {children}
-            
-            <footer className={styles.footer}></footer>
-            </main>
+            <main >
+              {children}
+            </main>  
         </div>
+        <footer className={styles.footer}></footer>
       </>
   )
 }
--- components/Nav.js
+++
@@ -1,7 +0,0 @@
-// import navStyles from '../styles/Nav.modules.css'
-
-// export default function Nav() {
-//   return (
-//     <nav className={navStyles.Nav}>Nav</nav>
-//   )
-// }
